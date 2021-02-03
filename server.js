1302a6050ee8095f7d48db735d9865a10ce9d95b
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-02-03 00:21:03 -0600

    Added delete function

--- server.js
+++ server.js
@@ -8,40 +8,51 @@ var PORT = process.env.port || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("./Develop/public"));
 
 //HTML Routes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
-  });
+});
 
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/Develop/db/db.json"));
-  });
+});
 
-  
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
-  });
+});
 
-
-
-  app.post("/api/notes", function(req, res) {
+//Function to POST notes to DB
+app.post("/api/notes", function(req, res) {
     const userNote = req.body
     const data = fs.readFileSync(__dirname + '/Develop/db/db.json')
     const product = JSON.parse(data)
     userNote.id = product.length + 1;
     product.push(userNote)
-        fs.writeFile(__dirname + "/Develop/db/db.json", JSON.stringify(product), "utf-8", function(err){
-            if (err) throw err
-            console.log("success");
-        })
-  
+    fs.writeFile(__dirname + "/Develop/db/db.json", JSON.stringify(product), "utf-8", function(err) {
+        if (err) throw err
+        console.log("success");
+    })
+
     res.json(userNote);
-  });
-  
-  // Starts the server to begin listening
-  app.listen(PORT, function() {
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    const remove = req.params.id;
+    const data = fs.readFileSync(__dirname + "/Develop/db/db.json");
+    const product = JSON.parse(data)
+
+    for (let i = 0; i < product.length; i++) {
+        if (product[i].id === remove) {
+            product.remove(product[i]);
+            console.log("Removed");
+        }
+        //.splice?
+    }
+})
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
     console.log("App listening on http://localhost:" + PORT);
-  });
+});
