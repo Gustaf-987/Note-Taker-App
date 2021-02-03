c1e7ec3db32fa1c475e03f4373b05cc0e4673d01
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-02-02 22:44:48 -0600

    notes write to page succesfully

--- server.js
+++ server.js
@@ -11,46 +11,37 @@ app.use(express.json());
 
 app.use(express.static("./Develop/public"));
 
+//HTML Routes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
   });
-  
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
-  });
-  
-  // Displays all characters
-  app.get("/api/notes", function(req, res) {
-    return res.json(characters);
-  });
-  
-  // Displays a single character, or returns false
-  app.get("/api/notes/:notes", function(req, res) {
-    var chosen = req.params.character;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < notes.length; i++) {
-      if (chosen === notes[i].routeName) {
-        return res.json(characters[i]);
-      }
-    }
-  
-    return res.json(false);
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/Develop/db/db.json"));
   });
 
-  app.post("/api/notes", function(req, res) {
-    let newNote = req.body
-    let rawData = fs.readFileSync('/Develop/db/db.json')
-    let result = JSON.parse(rawData)
   
-    result.push(newNote)
-    console.log(result);
-    res.json(result);
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+  });
+
+
+
+  app.post("/api/notes", function(req, res) {
+    const userNote = req.body
+    const data = fs.readFileSync(__dirname + '/Develop/db/db.json')
+    const product = JSON.parse(data)
+    userNote.id = product.length + 1;
+    product.push(userNote)
+        fs.writeFile(__dirname + "/Develop/db/db.json", JSON.stringify(product), "utf-8", function(err){
+            if (err) throw err
+            console.log("success");
+        })
+  
+    res.json(userNote);
   });
   
   // Starts the server to begin listening
-  // =============================================================
   app.listen(PORT, function() {
-    console.log("App listening on http://localhost: " + PORT);
+    console.log("App listening on http://localhost:" + PORT);
   });
